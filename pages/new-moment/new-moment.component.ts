9c9096acd550b6290d39128baf73c342109ec400
welles <eduardogomesk123@live.com>
2024-10-01 19:42:32 -0300

    primeiro commit

---
+++ pages/new-moment/new-moment.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-moment',
+  templateUrl: './new-moment.component.html',
+  styleUrl: './new-moment.component.css'
+})
+export class NewMomentComponent {
+
+  btnText = "Compartilhar!";
+
+}
